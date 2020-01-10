c747af138c9cd3a853fa3571cc06abc9ba5791ee
Paul Evans <paulevans89@gmail.com>
2020-01-10 16:20:00 +0000

    changed transition effect of the burger menu

--- menu.js
+++ menu.js
@@ -2,6 +2,9 @@ let nav = document.querySelector('nav ul');
 let main = document.querySelector('main');
 let footer = document.querySelector('footer');
 let menu = document.querySelector('.burger');
+let opaque = document.querySelector('.menu-active-opaque');
+
+
 
 
 menu.addEventListener('click', activateMenu);
@@ -12,4 +15,5 @@ function activateMenu() {
 	main.classList.toggle('main-active');
 	footer.classList.toggle('main-active');
 	menu.classList.toggle('menu-active');
+	opaque.classList.toggle('opaque');
 }
