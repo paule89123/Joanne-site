d62f1c73a49189a7e56f75ae78d8d324e4eb103c
Paul Evans <paulevans89@gmail.com>
2020-01-10 22:31:34 +0000

    fixed several bugs with the burger menu that arose when the window was resized

--- menu.js
+++ menu.js
@@ -1,19 +1,25 @@
-let nav = document.querySelector('nav ul');
+let nav = document.querySelector('.navMobile');
 let main = document.querySelector('main');
 let footer = document.querySelector('footer');
 let menu = document.querySelector('.burger');
-let opaque = document.querySelector('.menu-active-opaque');
-
-
-
 
 menu.addEventListener('click', activateMenu);
-
 
 function activateMenu() {
 	nav.classList.toggle('nav-active');
 	main.classList.toggle('main-active');
 	footer.classList.toggle('main-active');
 	menu.classList.toggle('menu-active');
-	opaque.classList.toggle('opaque');
+}
+
+window.addEventListener("resize", removeActiveClasses);
+
+function removeActiveClasses(e) {
+	let width = document.body.clientWidth;
+  	if (width > 600) {
+		nav.classList.remove('nav-active');
+		main.classList.remove('main-active');
+		footer.classList.remove('main-active');
+		menu.classList.remove('menu-active');
+	}
 }
